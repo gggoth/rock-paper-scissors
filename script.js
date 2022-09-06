c15098cd1c7fe883c6a64b68f1e17f4db30c4595
lu <umeanlikethat@protonmail.com>
2022-09-06 14:06:34 +0200

    added computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("hello rock paper scissors");
+
+/* 
+this function gets the choice of the computer and
+0 = rock
+1 = paper
+2 = scissors
+*/
+
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random()* 3);
+    return computerChoice;
+}
