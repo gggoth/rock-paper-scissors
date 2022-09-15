4f1a33bd4e69983cf05d0dde0d5d4120a537955b
lu <umeanlikethat@protonmail.com>
2022-09-15 16:02:43 +0200

    functional buttons and score

--- script.js
+++ script.js
@@ -8,12 +8,173 @@ this function gets the choice of the computer and
 */
 
 let scoreComputer = 0;
-let scorePlayer = 0;
+let scorePlayer = 0; 
 
-function game(){
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let PlayerSelection = button.id;
+        let computerSelection = getComputerChoice(); 
+        console.log(PlayerSelection);
+        console.log(computerSelection);
+        
+        
+        playRound(PlayerSelection, computerSelection);
+        console.log(scorePlayer);
+        console.log(scoreComputer);
+           
+        
+    })
+})
 
+
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random()* 3);
     
+    if (computerChoice == 0) {
+        return "rock";
+        }
+        else if (computerChoice == 1) {
+            return "paper";
+            }
+        else {
+            return "scissors";
+        }
+}
 
+function playRound(PlayerSelection, computerSelection) {
+    
+    if (PlayerSelection == "rock" && computerSelection == "paper") {
+        scoreComputer = ++scoreComputer;
+        alert("you lose!");
+    }
+        else if (PlayerSelection == "rock" && computerSelection == "rock") {
+            alert("you tie!");
+        }
+    
+        else if (PlayerSelection == "rock" && computerSelection == "scissors") {
+            scorePlayer = ++scorePlayer;
+            alert("you win!");
+             }
+
+        else if (PlayerSelection == "paper" && computerSelection == "scissors") {
+            scoreComputer = ++scoreComputer;
+            alert("you lose!");
+        }
+
+        else if (PlayerSelection == "paper" && computerSelection == "rock") {
+            scorePlayer = ++scorePlayer;
+            alert("you win!");
+        }
+
+        else if (PlayerSelection == "paper" && computerSelection == "paper") {
+            alert("you tie!");
+        }
+
+        else if (PlayerSelection == "scissors" && computerSelection == "scissors") {
+            alert("you tie!");
+        }
+
+        else if (PlayerSelection == "scissors" && computerSelection == "rock") {
+            scoreComputer = ++scoreComputer;
+            alert("you lose!");
+        }
+
+        else if (PlayerSelection == "scissors" && computerSelection == "paper") {
+            scorePlayer = ++scorePlayer;
+            alert("you win!");
+        }
+
+        else if (PlayerSelection != "scissors" && PlayerSelection != "rock" && PlayerSelection != "paper") {
+            return "sorry, your weapons are: rock, paper, scissors"
+        }
+    }
+
+
+
+
+
+/* const btn = document.querySelector('#rock');
+btn.addEventListener('click', function(e) {
+    console.log(e);
+});
+
+
+
+
+/* function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random()* 3);
+    
+    if (computerChoice == 0) {
+        return "rock";
+        }
+        else if (computerChoice == 1) {
+            return "paper";
+            }
+        else {
+            return "scissors";
+        }
+}
+
+let computerSelection = getComputerChoice();
+        console.log(computerSelection);
+
+
+function playRound(PlayerSelection, computerSelection) {
+    
+    if (PlayerSelection == "rock" && computerSelection == "paper") {
+        scoreComputer = ++scoreComputer;
+        alert("you lose!");
+    }
+        else if (PlayerSelection == "rock" && computerSelection == "rock") {
+            alert("you tie!");
+        }
+    
+        else if (PlayerSelection == "rock" && computerSelection == "scissors") {
+            scorePlayer = ++scorePlayer;
+            alert("you win!");
+             }
+
+        else if (PlayerSelection == "paper" && computerSelection == "scissors") {
+            scoreComputer = ++scoreComputer;
+            alert("you lose!");
+        }
+
+        else if (PlayerSelection == "paper" && computerSelection == "rock") {
+            scorePlayer = ++scorePlayer;
+            alert("you win!");
+        }
+
+        else if (PlayerSelection == "paper" && computerSelection == "paper") {
+            alert("you tie!");
+        }
+
+        else if (PlayerSelection == "scissors" && computerSelection == "scissors") {
+            alert("you tie!");
+        }
+
+        else if (PlayerSelection == "scissors" && computerSelection == "rock") {
+            scoreComputer = ++scoreComputer;
+            alert("you lose!");
+        }
+
+        else if (PlayerSelection == "scissors" && computerSelection == "paper") {
+            scorePlayer = ++scorePlayer;
+            alert("you win!");
+        }
+
+        else if (PlayerSelection != "scissors" && PlayerSelection != "rock" && PlayerSelection != "paper") {
+            return "sorry, your weapons are: rock, paper, scissors"
+        }
+    }
+
+
+*/
+
+
+
+
+/* function game(){
     for (let i = 1; i <= 5; i++) {
     
         function playRound(PlayerSelection, computerSelection) {
@@ -104,4 +265,4 @@ function finalScore() {
 }
 
 console.log(game());
-console.log(finalScore());
+console.log(finalScore()); */
